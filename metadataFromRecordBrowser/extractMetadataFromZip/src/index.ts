ba0a4ab8fc744c907d5701819c59dad3514f1ce7
Sebastian Gurin <sebastigurin@gmail.com>
2019-01-29 22:06:56 -0300

    extracted .json

--- metadataFromRecordBrowser/extractMetadataFromZip/src/index.ts
+++ metadataFromRecordBrowser/extractMetadataFromZip/src/index.ts
@@ -9,7 +9,7 @@ interface Config {
   outputFolder: string
 }
 export async function extractAll(config: Config): Promise<void> {
-  const tool = await new Tool()
+  const tool = new Tool()
   return await tool.extractAll(config)
 }
 
@@ -17,70 +17,66 @@ class Tool {
   config!: Config;
   browser!: Browser;
   page!: Page;
-  metadataFromRecordBrowserCode!: string;
 
   public async extractAll(config: Config) {
-    this.metadataFromRecordBrowserCode = await readFileSync('../metadataFromRecordBrowser.js').toString()
-    this.config = await config
+    this.config = config
     this.browser = await launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     this.page = await this.browser.newPage();
     await this.page.goto(this.config.startUrl)
     await this.visitAllPages(async data => {
-      await writeFileSync(this.config.outputFolder + '/' + data.id + '.json', JSON.stringify(data, null, 2))
+      writeFileSync(this.config.outputFolder + '/' + data.id + '.json', JSON.stringify(data, null, 2))
     })
     await this.browser.close()
   }
 
-
   protected async extractCurrentPageRecord(): Promise<any> {
-    const fnString = `(()=>{${await this.metadataFromRecordBrowserCode}; return JSON.stringify(getRecord(), null, 2) })()`
-    const data = await JSON.parse(await this.page.evaluate(fnString))
-    return await data
+    const fnString = `(()=>{${readFileSync('../metadataFromRecordBrowser.js').toString()}; return JSON.stringify(getRecord(), null, 2) })()`
+    const data = JSON.parse(await this.page.evaluate(fnString))
+    return data
   }
 
   protected async visitAllPageRecords(fn: (data: any) => Promise<void>) {
-    await this.page.waitFor(await '#contentPanel h1')
-    await this.page.waitFor(await '#recordlist button[name="recordswitch"]')
-    let recordCount: number = await this.page.evaluate(() => jQuery('#recordlist button[name="recordswitch"]').length)
+    await this.page.waitFor('#contentPanel h1')
+    await this.page.waitFor('#recordlist button[name="recordswitch"]')
+    let recordCount: number = await this.page.evaluate(() => jQuery('#recordlist button[name="recordswitch"]').length) +1
     // console.log('visitAllPageRecords',recordCount);
-    let currentRecord: string = await ''
-    for (let i = await 0; i < await recordCount; await i++) {
+    let currentRecord: string = ''
+    for (let visitAllPageRecordIndex = 0; visitAllPageRecordIndex < recordCount; visitAllPageRecordIndex++) {
       await this.page.waitFor('#contentPanel h1')
       await this.page.waitFor('#recordlist button[name="recordswitch"]')
       try {
         const data = await this.extractCurrentPageRecord()
-        await fn(await data)
-        await console.log('extractCurrentPageRecord ', await data.id, await data.label, await data.fields.length, await data.sublists.length, await data.searchFilters.length);
+        await fn(data)
+        console.log('extractCurrentPageRecord ', visitAllPageRecordIndex, data.id, data.label, data.fields.length, data.sublists.length, data.searchFilters.length);
       } catch (error) {
-        await console.error('ERROR on extractPage for ' + currentRecord, error);
+        console.error('ERROR on extractPage for ' + currentRecord, error);
       }
       // console.log(data.fields.length, data.sublists.length)
-      currentRecord = await this.page.evaluate((i) => jQuery('#recordlist button[name="recordswitch"]').arr()[i].attr('id'), await i)
+      currentRecord = await this.page.evaluate((visitAllPageRecordIndex_) => jQuery('#recordlist button[name="recordswitch"]').arr()[visitAllPageRecordIndex_].attr('id'), visitAllPageRecordIndex)
       // console.log(currentRecord, data.id, data.fields.length, data.sublists.length, data.searchFilters.length);
-      await this.page.click(await `#${await currentRecord}`)
-      
-      await this.page.reload(await {waitUntil: await 'networkidle2'})
-      await this.page.waitFor(await '#contentPanel h1')
-      await this.page.waitFor(await '#recordlist button[name="recordswitch"]')
+      await this.page.click(`#${currentRecord}`)
+      // await this.page.reload({waitUntil: 'networkidle0'})
+      await this.page.waitFor('#contentPanel h1')
+      await this.page.waitFor('#recordlist button[name="recordswitch"]')
     }
   }
   protected async visitAllPages(fn: (data: any) => Promise<void>) {
-    await this.page.waitFor(await '#contentPanel h1')
-    await this.page.waitFor(await '#alphabetrecordswitch button[name="alphabetswitch"]')
-    let letterCount: number = await this.page.evaluate(await (() => jQuery('#alphabetrecordswitch button[name="alphabetswitch"]').length))
+    await this.page.waitFor('#contentPanel h1')
+    await this.page.waitFor('#alphabetrecordswitch button[name="alphabetswitch"]')
+    let letterCount: number = await this.page.evaluate(() => jQuery('#alphabetrecordswitch button[name="alphabetswitch"]').length)
     // console.log(letterCount);
-    let currentLetter: string = await ''
-    for (let j = await 0; await j < await letterCount; await j++) {
-      await this.page.waitFor(await '#contentPanel h1')
-      await this.page.waitFor(await '#alphabetrecordswitch button[name="alphabetswitch"]')
-      currentLetter = await this.page.evaluate((j) => { jQuery.fn.arr = function () { return this.toArray().map(e => jQuery(e)) }; return jQuery('#alphabetrecordswitch button[name="alphabetswitch"]').arr()[j].attr('id') }, j)
-      await console.log(await `letter ${await currentLetter}`)
-      await this.visitAllPageRecords(await fn)
+    let currentLetter: string = ''
+    for (let visitAllPagesIndex = 0; visitAllPagesIndex < letterCount; visitAllPagesIndex++) {
+      await this.page.waitFor('#contentPanel h1')
+      await this.page.waitFor('#alphabetrecordswitch button[name="alphabetswitch"]')
+      currentLetter = await this.page.evaluate((visitAllPagesIndex_) => { jQuery.fn.arr = function () { return this.toArray().map(e => jQuery(e)) }; return jQuery('#alphabetrecordswitch button[name="alphabetswitch"]').arr()[visitAllPagesIndex_].attr('id') }, visitAllPagesIndex)
+      console.log('letter '+currentLetter)
+      await this.visitAllPageRecords(fn)
       // console.log('letter '+currentLetter);
-      await this.page.click(await `#${await currentLetter}`)
-      await this.page.reload(await {waitUntil: await 'networkidle2'})
-      await this.page.waitFor(await '#contentPanel h1')
-      await this.page.waitFor(await '#alphabetrecordswitch button[name="alphabetswitch"]')
+      await this.page.click(`#${currentLetter}`)
+      // await this.page.reload({waitUntil: 'networkidle0'})
+      await this.page.waitFor('#contentPanel h1')
+      await this.page.waitFor('#alphabetrecordswitch button[name="alphabetswitch"]')
     }
     await this.browser.close()
   }

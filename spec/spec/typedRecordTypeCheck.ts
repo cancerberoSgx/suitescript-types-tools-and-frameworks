6b93395791af06a05802ef6c63fe58322a682058
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-01 03:10:09 -0300

    spec

--- spec/spec/typedRecordTypeCheck.ts
+++ spec/spec/typedRecordTypeCheck.ts
@@ -1,59 +1,59 @@
-// import { expectType } from 'tsd-check';
-// import { load, loadOrThrow } from "../../src/record/typedRecord/typedRecordOperations";
-// import { Record } from "../../src/record/typedRecord/typedRecord";
-// import { CannotBeUndefined, CanBeUndefined, NotExtends, Extends, Identical } from './typeCheckUtil';
+import { expectType } from 'tsd-check';
+import { load, loadOrThrow } from "../../src/record/typedRecord/typedRecordOperations";
+import { Record } from "../../src/record/typedRecord/typedRecord";
+import { CannotBeUndefined, CanBeUndefined, NotExtends, Extends, Identical } from './typeCheckUtil';
 
-// describe('typedRecord', () => {
-//   describe('operations', () => {
-//     it('loadOrThrow', () => {
-//       const r = loadOrThrow({ id: 33, type: 'commercecategory' })
-//       type t = typeof r
-//       expectType<CannotBeUndefined<t>>(true)
-//       expectType<Record<'commercecategory'>>(r)
-//       expectType<'commercecategory'>(r.type)
-//     })
-//     it('load', () => {
-//       const r = load({ id: 33, type: 'commercecategory' })
-//       type t = typeof r
-//       expectType<Record<'commercecategory'> | undefined>(r)
-//       expectType<CanBeUndefined<t>>(true)
-//       expectType<'commercecategory'>(r!.type)
-//     })
-//     xit('create', () => { })
-//   })
+describe('typedRecord', () => {
+  describe('operations', () => {
+    it('loadOrThrow', () => {
+      const r = loadOrThrow({ id: 33, type: 'commercecategory' })
+      type t = typeof r
+      expectType<CannotBeUndefined<t>>(true)
+      expectType<Record<'commercecategory'>>(r)
+      expectType<'commercecategory'>(r.type)
+    })
+    it('load', () => {
+      const r = load({ id: 33, type: 'commercecategory' })
+      type t = typeof r
+      expectType<Record<'commercecategory'> | undefined>(r)
+      expectType<CanBeUndefined<t>>(true)
+      expectType<'commercecategory'>(r!.type)
+    })
+    xit('create', () => { })
+  })
 
-//   describe('fields', () => {
-//     it('getFields', () => {
-//       const fields = loadOrThrow({ id: 33, type: 'commercecategory' }).getFields()
-//       type t = typeof fields
-//       expectType<CannotBeUndefined<t>>(true)
-//       expectType<NotExtends<string[], t>>(true)
-//       expectType<Extends<string[], t>>(false)
-//       expectType<Extends<["addtohead", "catalog", "created", "description", "displayinsite", "externalid", "isinactive", "lastmodified", "lastmodifiedby", "metadescription", "metakeywords", "version"], t>>(true)
-//       expectType<NotExtends<["strangeeee", "addtohead", "catalog", "created", "description", "displayinsite", "externalid", "isinactive", "lastmodified", "lastmodifiedby", "metadescription", "metakeywords", "version"], t>>(true)
-//     })
+  describe('fields', () => {
+    it('getFields', () => {
+      const fields = loadOrThrow({ id: 33, type: 'commercecategory' }).getFields()
+      type t = typeof fields
+      expectType<CannotBeUndefined<t>>(true)
+      expectType<NotExtends<string[], t>>(true)
+      expectType<Extends<string[], t>>(false)
+      expectType<Extends<["addtohead", "catalog", "created", "description", "displayinsite", "externalid", "isinactive", "lastmodified", "lastmodifiedby", "metadescription", "metakeywords", "version"], t>>(true)
+      expectType<NotExtends<["strangeeee", "addtohead", "catalog", "created", "description", "displayinsite", "externalid", "isinactive", "lastmodified", "lastmodifiedby", "metadescription", "metakeywords", "version"], t>>(true)
+    })
 
-//     it('getField', () => {
-//       const field = loadOrThrow({ id: 33, type: 'commercecategory' }).getField({fieldId: 'created'})
-//       type t = typeof field
-//       expectType<CanBeUndefined<t>>(true)
-//       if(!field){return}
-//       const fieldId = field.id
-//       type fieldT = typeof fieldId
-//       expectType<Identical<'created', fieldT>>(true)
-//       expectType<'created'>(fieldId)
-//       expectType<Extends<string, fieldT>>(false)
+    it('getField', () => {
+      const field = loadOrThrow({ id: 33, type: 'commercecategory' }).getField({fieldId: 'created'})
+      type t = typeof field
+      expectType<CanBeUndefined<t>>(true)
+      if(!field){return}
+      const fieldId = field.id
+      type fieldT = typeof fieldId
+      expectType<Identical<'created', fieldT>>(true)
+      expectType<'created'>(fieldId)
+      expectType<Extends<string, fieldT>>(false)
 
 
-//       expectType<Identical<Date, typeof field.type>>(true)
+      expectType<Identical<Date, typeof field.type>>(true)
 
-//       // var fieldType = field.type
+      // var fieldType = field.type
 
-//       // type fieldTypeT  = NonNullable<string>
-// // 
-//       // expectType<Extends<"string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function", typeof field.type>>(true)
+      // type fieldTypeT  = NonNullable<string>
+// 
+      // expectType<Extends<"string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function", typeof field.type>>(true)
 
-//       // expectType<Extends<string, typeof field.type>>(false)
-//     })
-//   })
-// })
+      // expectType<Extends<string, typeof field.type>>(false)
+    })
+  })
+})
--- spec/spec/typedSearchTypeCheck.ts
+++ spec/spec/typedSearchTypeCheck.ts
@@ -1,27 +1,27 @@
-// import { expectType } from 'tsd-check';
-// import { Result, search } from "../../src/search/typedSearch/typedSearch";
-// import { CanBeUndefined, Identical } from './typeCheckUtil';
+import { expectType } from 'tsd-check';
+import { Result, search } from "../../src/search/typedSearch/typedSearch";
+import { CanBeUndefined, Identical } from './typeCheckUtil';
 
-// describe('typedsearch', () => {
-//   describe('operations', () => {
-//     it('loadOrThrow', () => {
-//       const s3 = search({ type: 'inventoryitem', columns: ['currentstdcosteffectivedate'] });
+describe('typedsearch', () => {
+  describe('operations', () => {
+    it('loadOrThrow', () => {
+      const s3 = search({ type: 'inventoryitem', columns: ['currentstdcosteffectivedate'] });
 
-//       expectType<CanBeUndefined<typeof s3>>(false)
-//       expectType<Identical<'inventoryitem', typeof s3.searchType>>(true)
+      expectType<CanBeUndefined<typeof s3>>(false)
+      expectType<Identical<'inventoryitem', typeof s3.searchType>>(true)
 
-//       const i2 = s3.run().each(f => {
-//         expectType<Date|undefined>(f.getValue('dateviewed'))
-//         expectType<number|undefined>(f.getValue('safetystocklevel'))
-//         expectType<boolean|undefined>(f.getValue('isavailable'))
-//         return false;
-//       });
-//       expectType<Result<'inventoryitem'>|undefined>(i2)
-//       expectType<Date|undefined>(i2!.getValue('dateviewed'))
-//       expectType<number|undefined>(i2!.getValue('safetystocklevel'))
-//       expectType<boolean|undefined>(i2!.getValue('isavailable'))
+      const i2 = s3.run().each(f => {
+        expectType<Date|undefined>(f.getValue('dateviewed'))
+        expectType<number|undefined>(f.getValue('safetystocklevel'))
+        expectType<boolean|undefined>(f.getValue('isavailable'))
+        return false;
+      });
+      expectType<Result<'inventoryitem'>|undefined>(i2)
+      expectType<Date|undefined>(i2!.getValue('dateviewed'))
+      expectType<number|undefined>(i2!.getValue('safetystocklevel'))
+      expectType<boolean|undefined>(i2!.getValue('isavailable'))
       
-//     })
-//   })
+    })
+  })
 
-// })
+})

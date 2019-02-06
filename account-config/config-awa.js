5fbd3c92ca72d48610d6e195aba2002a6ecdb174
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-05 21:18:06 -0300

    ignore

--- account-config/config-awa.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    account: 'TSTDRV1844288',
-    role: 3,
-    folderPath: 'SuiteScripts',
-    applicationId: '7C642855-8B3E-4DAA-B7A2-CAEFB92365C0',
-    nsVersion: '2017_2',
-    uploadConcurrency: 1
-}

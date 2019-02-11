37cd045a3f020afad39f0adf6da72444b3f8053c
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-11 05:41:45 -0300

    removed heroes and teams

--- react-redux-client-app/src/routes.tsx
+++ react-redux-client-app/src/routes.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import HeroesPage from './pages/heroes';
 import IndexPage from './pages/index';
 import ListRecordTypesPage from './pages/listRecordTypes';
-import TeamsPage from './pages/teams';
 import Header from './components/layout/Header';
 import recordView from './pages/recordView';
 
 const Routes = () => (
   <div>
-    <Header title="Example App" />
+    <Header title="Mini NetSuite UI" />
     <HashRouter>
       <Switch>
         <Route exact path="/" component={IndexPage} />
         <Route path="/listRecordTypes/:type" component={ListRecordTypesPage} />
         <Route path="/listRecordTypes" component={ListRecordTypesPage} />
-        {/* <Route path="/recordView/:type/:id" component={recordView} /> */}
         <Route path="/recordView/:type/:id/:options" component={recordView} />
-        <Route path="/heroes" component={HeroesPage} />
-        <Route path="/teams" component={TeamsPage} />
         <Route component={() => <div>Not Found</div>} />
       </Switch>
     </HashRouter>
